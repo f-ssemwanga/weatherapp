19cde36f17f93560bb395dbad2adf15dffbb5174
LordNinth <rr621801@gmail.com>
2023-05-16 00:00:45 +0100

    basic skeleton

---
+++ ap.js
@@ -0,0 +1,33 @@
+//openweather map api key / url
+const apiKey = "6666be5ff325814a0b88abedf7bb245c";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+
+//user input are captured in this search box
+const searchInput  = document.querySelector(".search input");
+const searchBtn  = document.querySelector(".search");
+
+
+//function to pull data from api and displaying on web ap
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`); //fecting api url + apiKey
+    const data = await response.json(); 
+
+    console.log(data); 
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = Math.round(data.wind.speed) + "km/hr";
+}
+
+//calling function
+checkWeather();
+
+//event listener to search button
+searchBtn.addEventListener("click",() => {
+
+    checkWeather(searchInput.value);
+})
+
+
